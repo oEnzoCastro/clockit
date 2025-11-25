6d074ec963d05866db1ca290cbf0fdacffc0eb0a
oEnzoCastro <enzocastru@gmail.com>
2025-11-25 13:21:16 -0300

    feat: implement Background component with styling for enhanced visual design style: update globals.css for improved layout and spacing

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,29 +1,32 @@
-import { Poppins } from 'next/font/google'
-import './globals.css'
-import Header from "../components/Header/Header"
-
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import Header from "../components/Header/Header";
+import Background from "@/components/Background/Background";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '600', '700'], // pesos que vai usar
-})
+  subsets: ["latin"],
+  weight: ["400", "600", "700"], // pesos que vai usar
+});
 
 export const metadata = {
-  title: 'Meu Site',
-  description: 'Portfólio com Poppins',
-}
+  title: "Meu Site",
+  description: "Portfólio com Poppins",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
-      <body className={poppins.className}>
-        <Header />
+      <body
+        className={poppins.className}
+        style={{ position: "relative", height: "100vh" }}
+      >
         {children}
+        <Background />
       </body>
     </html>
-  )
+  );
 }
