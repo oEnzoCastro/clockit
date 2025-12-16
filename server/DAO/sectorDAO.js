d443a19157657ee15b2fcacc47f80f5e4a949f1d
MateusViggiani <mateus.viggiani@gmail.com>
2025-12-16 20:10:36 -0300

    refactored sectorDAO

--- server/DAO/sectorDAO.js
+++ server/DAO/sectorDAO.js
@@ -1,8 +1,5 @@
 const AreaDAO = require('./areaDAO');
-const Area = require('../models/area');
 const Sector = require('../models/sector');
-//const db = require('../database/db');
-
 
 
 class SectorDAO {
@@ -13,15 +10,12 @@ class SectorDAO {
 
     async create(sector) {
         const trx = await this.db.transaction();
-
         try {
             if (!(sector instanceof Sector)) {
                 throw new Error("sector must be an instance of Sector");
             }
 
-
             const area = await this.areaDAO.getAreaById(sector.area_id);
-
             if (!area) {
                 throw new Error("Area not found for the given area_id");
             }
@@ -49,6 +43,82 @@ class SectorDAO {
         }
     }
 
+    async findSectors(filters = {}, trx = this.db) {
+        try {
+            const {
+                id,
+                sector_name,
+                acronym,
+                area_id,
+                institute_id,
+                agent_id
+            } = filters;
+
+            let query = trx('sector as a').select('a.*');
+
+            if (institute_id) {
+                query = query
+                    .join('area as b', 'a.area_id', 'b.id')
+                    .where('b.institute_id', institute_id);
+            }
+
+            if (agent_id) {
+                query = query
+                    .join('agent_sector as c', 'a.id', 'c.sector_id')
+                    .where('c.agent_id', agent_id);
+            }
+
+            if (id) query.where('a.id', id);
+            if (sector_name) query.where('a.sector_name', sector_name);
+            if (acronym) query.where('a.acronym', acronym);
+            if (area_id) query.where('a.area_id', area_id);
+
+            const rows = await query;
+            return rows.map(row => new Sector(row));
+
+        } catch (error) {
+            console.error("Error in findSectors():", error);
+            throw error;
+        }
+    }
+
+
+    async getSectorById(id) {
+        const res = await this.findSectors({ id });
+        return res[0] || null;
+    }
+
+    async getSectorsByArea(area_id) {
+        return this.findSectors({ area_id });
+    }
+
+    async getSectorsByAcronymArea(acronym, area_id) {
+        const res = await this.findSectors({ acronym, area_id });
+        return res[0] || null;
+    }
+
+    async getSectorsByNameArea(sector_name, area_id) {
+        const res = await this.findSectors({ sector_name, area_id });
+        return res[0] || null;
+    }
+
+    async getSectorsByInstitute(institute_id) {
+        return this.findSectors({ institute_id });
+    }
+
+    async getSectorsByNameInstitute(institute_id, sector_name) {
+        return this.findSectors({ institute_id, sector_name });
+    }
+
+    async getSectorsByAcronymInstitute(institute_id, acronym) {
+        return this.findSectors({ institute_id, acronym });
+    }
+
+    async getSectorsByAgentId(agent_id, trx = this.db) {
+        return this.findSectors({ agent_id }, trx);
+    }
+
+    
     async update(sector) {
         const trx = await this.db.transaction();
         try {
@@ -58,9 +128,15 @@ class SectorDAO {
 
             const { id, sector_name, acronym, is_hidden } = sector.toJSON();
 
+            const existingSectorAcronym = await trx('sector')
+                .where({ acronym })
+                .whereNot({ id })
+                .first();
 
-            const existingSectorAcronym = await trx('sector').where({ acronym }).whereNot({ id }).first();
-            const existingSectorName = await trx('sector').where({ sector_name }).whereNot({ id }).first();
+            const existingSectorName = await trx('sector')
+                .where({ sector_name })
+                .whereNot({ id })
+                .first();
 
             if (existingSectorAcronym) {
                 throw new Error("This acronym is already used by another sector");
@@ -68,12 +144,16 @@ class SectorDAO {
             if (existingSectorName) {
                 throw new Error("This name is already used by another sector");
             }
-            let updatedSector;
-            if (is_hidden) {
-                [updatedSector] = await trx('sector').where({ id: id }).update({ sector_name, acronym, is_hidden }).returning('*');
-            } else {
-                [updatedSector] = await trx('sector').where({ id: id }).update({ sector_name, acronym }).returning('*');
-            }
+
+            const [updatedSector] = await trx('sector')
+                .where({ id })
+                .update(
+                    is_hidden !== undefined
+                        ? { sector_name, acronym, is_hidden }
+                        : { sector_name, acronym }
+                )
+                .returning('*');
+
             if (!updatedSector) {
                 throw new Error("Cannot update sector or sector does not exists");
             }
@@ -81,249 +161,158 @@ class SectorDAO {
             await trx.commit();
             return new Sector(updatedSector);
 
-
         } catch (error) {
             await trx.rollback();
-            console.error("Error occurred in update() SectorDAO:" + error);
+            console.error("Error occurred in update() SectorDAO:", error);
             throw error;
         }
     }
 
+    
     async delete(id) {
         const trx = await this.db.transaction();
         try {
-
             const result = await trx('sector').where({ id }).del();
             if (result > 0) {
                 await trx.commit();
                 return true;
-            } else {
-                await trx.rollback();
-                return false;
             }
+            await trx.rollback();
+            return false;
         } catch (error) {
             await trx.rollback();
-            console.error("Error occurred in Delete SectorDAO:" + error);
+            console.error("Error occurred in Delete SectorDAO:", error);
             throw error;
         }
     }
 
-    async getSectorById(id) {
-        try {
-            const sector = await this.db('sector').where({ id }).first();
-            if (!sector) {
-                return null;
-            }
-            return new Sector({ id: sector.id, area_id: sector.area_id, acronym: sector.acronym, sector_name: sector.sector_name, is_hidden: sector.is_hidden || false });
-        } catch (error) {
-            console.error("Error occurred in getSectorById() SectorDAO:" + error);
-            throw error;
-        }
+    
+    async exists(id, trx = this.db) {
+        const sector = await trx('sector').where({ id }).first();
+        return !!sector;
     }
 
-    async getSectorsByInstitute(institute_id) {
-        try {
-            const rows = await this.db('sector as a')
-                .join('area as b', 'a.area_id', 'b.id')
-                .select('a.*')
-                .where('b.institute_id', institute_id);
-            if (!rows || rows.length == 0) {
-                return [];
-            }
-
-            const sectors = rows.map(s => new Sector(s));
-
-            return sectors;
-
-        } catch (error) {
-            console.error("Error occurred in getSectorByInstitute() SectorDAO:" + error);
-            throw error;
-        }
+    async existsByNameArea(sector_name, area_id, trx = this.db) {
+        const sector = await trx('sector').where({ sector_name, area_id }).first();
+        return !!sector;
     }
 
-    async getSectorsByArea(area_id) {
-        try {
-            const rows = await this.db('sector').where({ area_id });
-            if (!rows || rows.length == 0) {
-                return [];
-            }
-            const sectors = rows.map(s => new Sector(s));
-            return sectors;
-
-        } catch (error) {
-
-        }
-    }
-    async getSectorsByAcronymArea(acronym, area_id) {
-        try {
-            const row = await this.db('sector').where({ area_id, acronym }).first();
-            if (!row) {
-                return null;
-            }
-            const sectors = new Sector(row);
-            return sectors;
-
-        } catch (error) {
-            console.error("Error occurred in getSectorsByAcronymArea = SectorDAO:" + error);
-            throw error;
-        }
-    }
-    async getSectorsByNameArea(sector_name, area_id) {
-        try {
-            const row = await this.db('sector').where({ area_id, sector_name }).first();
-            if (!row) {
-                return null;
-            }
-            const sectors = new Sector(row);
-            return sectors;
-
-        } catch (error) {
-            console.error("Error occurred in getSectorsByNameArea SectorDAO:" + error);
-            throw error;
-        }
+    async existsByAcronymInstitute(acronym, area_id, trx = this.db) {
+        const sector = await trx('sector').where({ acronym, area_id }).first();
+        return !!sector;
     }
 
-    async getSectorsByNameInstitute(institute_id, sector_name) {
-        try {
-            const rows = await this.db('sector as a')
-                .join('area as b', 'a.area_id', 'b.id')
-                .select('a.*')
-                .where('b.institute_id', institute_id)
-                .andWhere('a.sector_name', sector_name);
-            if (!rows || rows.length == 0) {
-                return [];
-            }
-
-            const sectors = rows.map(s => new Sector(s));
-
-            return sectors;
-
-        } catch (error) {
-            console.error("Error occurred in getSectorsByNameInstitute SectorDAO:" + error);
-            throw error;
-        }
-    }
-
-    async getSectorsByAcronymInstitute(institute_id, acronym) {
-        try {
-            const rows = await this.db('sector as a')
-                .join('area as b', 'a.area_id', 'b.id')
-                .select('a.*')
-                .where('b.institute_id', institute_id)
-                .andWhere('a.acronym', acronym);
-            if (!rows || rows.length == 0) {
-                return [];
-            }
-
-            const sectors = rows.map(s => new Sector(s));
-
-            return sectors;
-
-        } catch (error) {
-            console.error("Error occurred in getSectorsByAcronymInstitute SectorDAO:" + error);
-            throw error;
-        }
-    }
-
-    async getSectorsByAgentId(agent_id,trx=this.db) {
-        try {
-            const rows = await trx('sector as a')
-                .select('a.*')
-                .join('agent_sector as b', 'a.id', 'b.sector_id')
-                .where('b.agent_id', agent_id);
-            if (!rows || rows.length == 0) {
-                return [];
-            }
-
-            const sectors = rows.map(s => new Sector(s));
-
-            return sectors;
-
-        } catch (error) {
-            console.error("Error occurred in getSectorsByAcronymInstitute SectorDAO:" + error);
-            throw error;
-        }
-    }
-
-    async UpdateHiddenSector(id, hidden = true) {
-        const trx = await this.db.transaction()
+    
+    async updateHiddenSector(id, hidden = true) {
+        const trx = await this.db.transaction();
         try {
             const [updated] = await trx('sector')
                 .where({ id })
                 .update({ is_hidden: hidden })
                 .returning('*');
+
             if (!updated) {
                 throw new Error("Could not update sector or sector does not exist");
             }
+
             await trx.commit();
             return updated;
+
         } catch (error) {
             await trx.rollback();
             console.error("Error in UpdateHiddenSector:", error);
             throw error;
         }
     }
-
 }
 
 module.exports = SectorDAO;
-
 /*
 async function main() {
-    // 1️⃣ Initialize database connection (edit for your setup)
-
-
-    const dao = new SectorDAO(db);
+    const sectorDAO = new SectorDAO(db);
+    const areaDAO = new AreaDAO(db);
+    const instituteDAO = new InstituteDAO(db);
 
     try {
-        console.log("=== Testing SectorDAO ===");
-        
-        // 2️⃣ Create a test Sector
-        const sectorData = new Sector({
-            sector_name: "SecondTest",
-            acronym: "TSTWO",
-            area_id: "8a0458ee-c2ea-45fa-8913-fd91f2bc86f5", // use a valid UUID from your DB
+        console.log('=== GET INSTITUTE ===');
+        const institute = (await instituteDAO.getInstitutes())[0];
+
+        if (!institute) {
+            throw new Error('No institute found. Create one before testing SectorDAO.');
+        }
+
+        console.log('Using institute:', institute);
+
+        console.log('\n=== GET AREA ===');
+        const area = (await areaDAO.getAreasByInstitute(institute.id))[0];
+
+        if (!area) {
+            throw new Error('No area found. Create an area before testing SectorDAO.');
+        }
+
+        console.log('Using area:', area);
+
+        console.log('\n=== CREATE SECTOR ===');
+        const sector = new Sector({
+            sector_name: 'Artificial Intelligence',
+            acronym: 'AI',
+            area_id: area.id,
             is_hidden: false
         });
 
-        console.log("Creating sector...");
-        const created = await dao.create(sectorData);
-        console.log("✅ Created sector:", created);
+        const created = await sectorDAO.create(sector);
+        console.log('Created:', created);
 
-        // 3️⃣ Get by ID
-        console.log("\nFetching sector by ID...");
-        const fetched = await dao.getSectorById(created.id);
-        console.log("✅ Fetched sector:", fetched);
+        console.log('\n=== FIND BY ID ===');
+        const byId = await sectorDAO.getSectorById(created.id);
+        console.log(byId);
 
-        // 4️⃣ Update the sector
-        console.log("\nUpdating sector...");
-        fetched.sector_name = "Updated Test Sector 2";
-        const updated = await dao.update(fetched);
-        console.log("✅ Updated sector:", updated);
+        console.log('\n=== FIND BY AREA ===');
+        const byArea = await sectorDAO.getSectorsByArea(area.id);
+        console.log(byArea);
 
-        // 5️⃣ Hide the sector
-        console.log("\nHiding sector...");
-        const hidden = await dao.UpdateHiddenSector(updated.id, true);
-        console.log("✅ Sector hidden:", hidden.is_hidden);
+        console.log('\n=== FIND BY ACRONYM + AREA ===');
+        const byAcronymArea = await sectorDAO.getSectorsByAcronymArea('AI', area.id);
+        console.log(byAcronymArea);
 
-        // 6️⃣ List by Area
-        console.log("\nGetting sectors by area...");
-        const byArea = await dao.getSectorsByArea(created.area_id);
-        console.log(`✅ Found ${byArea.length} sector(s) in area.`);
-        
-        const byInstitute = await dao.getSectorsByAcronymArea('TSTWO',"8a0458ee-c2ea-45fa-8913-fd91f2bc86f5");
+        console.log('\n=== FIND BY NAME + AREA ===');
+        const byNameArea = await sectorDAO.getSectorsByNameArea('Artificial Intelligence', area.id);
+        console.log(byNameArea);
+
+        console.log('\n=== FIND BY INSTITUTE ===');
+        const byInstitute = await sectorDAO.getSectorsByInstitute(institute.id);
         console.log(byInstitute);
 
-        // 7️⃣ Delete the sector
+        console.log('\n=== UPDATE SECTOR ===');
+        created.sector_name = 'AI & Machine Learning';
+        created.acronym = 'AIML';
 
-        console.log("\n🎉 All tests completed successfully!");
+        const updated = await sectorDAO.update(created);
+        console.log(updated);
+
+        console.log('\n=== HIDE SECTOR ===');
+        const hidden = await sectorDAO.updateHiddenSector(updated.id, true);
+        console.log(hidden);
+
+        console.log('\n=== EXISTS ===');
+        const exists = await sectorDAO.exists(updated.id);
+        console.log('Exists?', exists);
+        
+        console.log('\n=== DELETE SECTOR ===');
+        const deleted = await sectorDAO.delete(updated.id);
+        console.log('Deleted?', deleted);
+
+        console.log('\n=== FIND AFTER DELETE ===');
+        const afterDelete = await sectorDAO.getSectorById(updated.id);
+        console.log(afterDelete);
 
     } catch (error) {
-        console.error("❌ Error during DAO test:", error);
+        console.error('Test failed:', error.message);
     } finally {
-        await db.destroy();
+        await db.destroy(); // IMPORTANT
     }
 }
 
 main();
 */
+
