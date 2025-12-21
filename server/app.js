e6f00b195ff64fc896c5b0d46cd87a8c94ba955f
MateusViggiani <mateus.viggiani@gmail.com>
2025-12-20 23:26:29 -0300

    connected router

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const areaRoutes = require("./routes/areaRoutes");
 const agentRoutes = require("./routes/agentRoutes");
 const agentSectorRoutes = require("./routes/agentSectorRoutes");
 const calendarRoutes = require("./routes/calendarRoutes");
+const dayScheduleRoutes = require("./routes/dayScheduleRoutes");
+const sectorRoutes = require("./routes/sectorRoutes");
 const app = express();
 
 
@@ -11,11 +13,13 @@ app.use(express.json());
 
 // Necessário para receber form-data / x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-app.use(auth);
-app.use(areaRoutes);
-app.use(agentRoutes);
-app.use(agentSectorRoutes);
-app.use(calendarRoutes);
+app.use('/auth',auth);
+app.use('/areas',areaRoutes);
+app.use('/agents',agentRoutes);
+app.use('/agentSectors',agentSectorRoutes);
+app.use('/calendar',calendarRoutes);
+app.use('/daySchedules',dayScheduleRoutes);
+app.use('/sectors',sectorRoutes);
 
 app.listen(3000,()=>{
     console.log("app running on localhost:3000");
